132cc380c67014d5a5110bd4edfed2e5cd89e578
Rick van Schalkwijk <rick.vanschalkwijk@transavia.com>
2019-08-14 16:14:25 +0200

    basic selection sort

--- SelectionSort/Program.cs
+++ SelectionSort/Program.cs
@@ -2,11 +2,46 @@
 
 namespace SelectionSort
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var array = new[] { 1, 43, 56, 76, 23, 43, 89, 123, 56, 0, 23, 12, 5, 8, 100 };
+            Console.WriteLine("The Initial array is:");
+            Print(array);
+
+            int temp, smallest;
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                smallest = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[smallest])
+                        smallest = j;
+                }
+
+                temp = array[smallest];
+                array[smallest] = array[i];
+                array[i] = temp;
+            }
+
+            Console.WriteLine("Sorted Array:");
+            Print(array);
+
+            Console.ReadLine();
+        }
+
+        private static void Print(int[] array)
+        {
+            int length = array.Length;
+
+            for (int i = 0; i < length; ++i)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            Console.WriteLine();
         }
     }
 }
